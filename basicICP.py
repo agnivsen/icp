0ec1724e0e703447aee590ac1f07798eed5920cc
agniva sengupta <asengupt@agniva.irisa.fr>
2017-02-01 10:57:08 +0100

    point-to-plane Gauss Newton added

--- basicICP.py
+++ basicICP.py
@@ -1,8 +1,38 @@
 # -*- coding: utf-8 -*-
+# basicICP.py
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# * Neither the name of the copyright holders nor the names of any
+#   contributors may be used to endorse or promote products derived
+#   from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 """
 Created on Mon Jan 23 18:08:58 2017
 
-@author: asengupt
+@author: agnivsen
+
+A basic Iterative Closes Point matching betwwen two set of point cloud, 
+                        or one point cloud and one point + normal pair.
+                        
+Basic point to plane matching has been done using a Least squares approach and a Gauss-Newton approach
+Point to point matching has been done using Gauss-Newton only
 """
 
 import numpy as np
@@ -50,6 +80,8 @@ def read_file_deformed(file_path):
 
 def icp_point_to_plane(source_points, dest_points,loop):
     """
+    Point to plane matching using least squares
+    
     source_points:  nx3 matrix of n 3D points
     dest_points: nx6 matrix of n 3D points + 3 normal vectors, which have been obtained by some rigid deformation of 'source_points'
     """
@@ -118,6 +150,8 @@ def icp_point_to_plane(source_points, dest_points,loop):
         
 def icp_point_to_point_lm(source_points, dest_points,initial,loop):
     """
+    Point to point matching using Gauss-Newton
+    
     source_points:  nx3 matrix of n 3D points
     dest_points: nx3 matrix of n 3D points, which have been obtained by some rigid deformation of 'source_points'
     initial: 1x6 matrix, denoting alpha, beta, gamma (the Euler angles for rotation and tx, ty, tz (the translation along three axis). 
@@ -181,8 +215,73 @@ def icp_point_to_point_lm(source_points, dest_points,initial,loop):
         icp_point_to_point_lm(source_points,dest_points,initial, loop)
         
         
-        
+def icp_point_to_plane_lm(source_points, dest_points,initial,loop):
+    """
+    Point to plane matching using Gauss Newton
     
+    source_points:  nx3 matrix of n 3D points
+    dest_points: nx6 matrix of n 3D points + 3 normal vectors, which have been obtained by some rigid deformation of 'source_points'
+    initial: 1x6 matrix, denoting alpha, beta, gamma (the Euler angles for rotation and tx, ty, tz (the translation along three axis). 
+                this is the initial estimate of the transformation between 'source_points' and 'dest_points'
+    loop: start with zero, to keep track of the number of times it loops, just a very crude way to control the recursion            
+                
+    """
+    
+    J = []
+    e = []
+    
+    for i in range (0,dest_points.shape[0]-1):
+        
+        #print dest_points[i][3],dest_points[i][4],dest_points[i][5]
+        dx = dest_points[i][0]
+        dy = dest_points[i][1]
+        dz = dest_points[i][2]
+        nx = dest_points[i][3]
+        ny = dest_points[i][4]
+        nz = dest_points[i][5]
+        
+        sx = source_points[i][0]
+        sy = source_points[i][1]
+        sz = source_points[i][2]
+        
+        alpha = initial[0][0]
+        beta = initial[1][0]
+        gamma = initial[2][0]
+        tx = initial[3][0]        
+        ty = initial[4][0]
+        tz = initial[5][0]
+        
+        a1 = (nz*sy) - (ny*sz)
+        a2 = (nx*sz) - (nz*sx)
+        a3 = (ny*sx) - (nx*sy)
+        a4 = nx
+        a5 = ny
+        a6 = nz
+        
+        _residual = (alpha*a1) + (beta*a2) + (gamma*a3) + (nx*tx) + (ny*ty) + (nz*tz) - (((nx*dx) + (ny*dy) + (nz*dz)) - ((nx*sx) + (ny*sy) + (nz*sz)))
+    
+        _J = np.array([a1, a2, a3, a4, a5, a6])
+        _e = np.array([_residual])
+        
+    J.append(_J)
+    e.append(_e)
+        
+    jacobian = np.array(J)
+    residual = np.array(e)
+    
+    update = -np.dot(np.dot(np.linalg.pinv(np.dot(np.transpose(jacobian),jacobian)),np.transpose(jacobian)),residual)
+    
+    #print update, initial
+    
+    initial = initial + update
+    
+    print np.transpose(initial)
+    
+    loop = loop + 1
+    
+    if(loop < 50):  # here lies the control variable, control the number of iteration from here
+    
+        icp_point_to_point_lm(source_points,dest_points,initial, loop)
 
 
 fileOriginal = '/home/asengupt/Documents/icp/data/original.xyz'
@@ -191,8 +290,19 @@ deformed = '/home/asengupt/Documents/icp/data/deformed.xyz'
 source_points = read_file_original(fileOriginal)
 dest_points_et_normal = read_file_deformed(deformed)
 
-#icp_point_to_plane(source_points,dest_points_et_normal,0)
-
 initial = np.array([[0.01], [0.05], [0.01], [0.001], [0.001], [0.001]])
 
-icp_point_to_point_lm(source_points,dest_points_et_normal,initial,0)
+# **********************************************************************
+# Uncomment one of the following lines to trigger the respective module:
+# **********************************************************************
+
+
+#icp_point_to_plane(source_points,dest_points_et_normal,0)
+
+#icp_point_to_point_lm(source_points,dest_points_et_normal,initial,0)
+
+icp_point_to_plane_lm(source_points,dest_points_et_normal,initial,0)
+
+
+
+
